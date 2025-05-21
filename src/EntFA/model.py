b60b5c59f339e15fa8a58d0c563a4522bad1dacf
Aditya Mayukh Som <adi.kg2@gmail.com>
2025-05-21 18:39:30 +0000

    removed rl training steps

--- src/EntFA/model.py
+++ src/EntFA/model.py
@@ -1,33 +1,39 @@
 import math
+
 import torch
 import torch.nn as nn
-
 from fairseq.data.data_utils import collate_tokens
 from transformers import BartTokenizer
 
 
 class ConditionalSequenceGenerator:
     """Conditional sequence generator for calculating prior and posterior probability."""
+
     def __init__(self, bart):
         self.bart = bart
-        self.tokenizer = BartTokenizer.from_pretrained("facebook/bart-large", local_files_only=False)
-        
+        self.tokenizer = BartTokenizer.from_pretrained(
+            "facebook/bart-large", local_files_only=False
+        )
+
         self.encode_func = bart.encode
         self.decode_func = bart.decode
         self.max_positions = bart.max_positions
-        if type(self.max_positions) == int:
+
+        if type(self.max_positions) is int:
             self.max_positions = [self.max_positions]
         self.encode_line = bart.task.source_dictionary.encode_line
-        
+
         self._initialize()
-    
+
     def _initialize(self):
         """Set BART model to evaluation mode."""
         self.bart.cuda()
         self.bart.eval()
         self.bart.half()
-        
-    def tokenize_target(self, input_str, left_pad=False, append_eos=False):
+
+    def tokenize_target(
+        self, input_str: str | list[str], left_pad=False, append_eos=False
+    ):
         """BPE-encode a sentence (or multiple sentences).
 
         Args:
@@ -38,9 +44,9 @@ class ConditionalSequenceGenerator:
             prev_output_tokens (torch.Tensor): [batch_size, length]
             target (torch.Tensor): [batch_size, length]
             tgt_lengths (torch.Tensor): [batch_size]
-            
+
         """
-        if type(input_str) == type(''):
+        if type(input_str) is str:
             input_str = [input_str]
 
         prev_ids, tgt_ids = [], []
@@ -49,22 +55,26 @@ class ConditionalSequenceGenerator:
             tokens = self.bart.bpe.encode(ins)  # <mask>: 1279 27932 29
             calibration = 1
             if len(tokens.split(" ")) > min(self.max_positions) - calibration:
-                tokens = " ".join(tokens.split(" ")[: min(self.max_positions) - calibration])
-            
+                tokens = " ".join(
+                    tokens.split(" ")[: min(self.max_positions) - calibration]
+                )
+
             if append_eos:
                 tokens = "<s> " + tokens
             prev_tokens = "</s> " + tokens
             tgt_tokens = tokens + " </s>"
-            
+
             tgt_ids.append(self.encode_line(tgt_tokens, append_eos=False).long())
             prev_ids.append(self.encode_line(prev_tokens, append_eos=False).long())
 
-        prev_output_tokens = collate_tokens(prev_ids, pad_idx=1, left_pad=left_pad).cuda()
+        prev_output_tokens = collate_tokens(
+            prev_ids, pad_idx=1, left_pad=left_pad
+        ).cuda()
         target = collate_tokens(tgt_ids, pad_idx=1, left_pad=left_pad).cuda()
         tgt_lengths = torch.sum(target != 1, dim=1).cuda()
 
         return prev_output_tokens, target, tgt_lengths
-        
+
     def tokenize(self, input_str, append_bos=False, append_eos=True, left_pad=True):
         """BPE-encode a sentence (or multiple sentences).
 
@@ -77,7 +87,7 @@ class ConditionalSequenceGenerator:
             input_ids (torch.Tensor): [batch_size, length]
             src_lengths (torch.Tensor): [batch_size]
         """
-        if type(input_str) == type(''):
+        if type(input_str) is str:
             input_str = [input_str]
 
         input_ids = []
@@ -85,7 +95,9 @@ class ConditionalSequenceGenerator:
             tokens = self.bart.bpe.encode(ins)  # <mask>: 1279 27932 29
             calibration = sum([append_bos, append_eos])
             if len(tokens.split(" ")) > min(self.max_positions) - calibration:
-                tokens = " ".join(tokens.split(" ")[: min(self.max_positions) - calibration])
+                tokens = " ".join(
+                    tokens.split(" ")[: min(self.max_positions) - calibration]
+                )
 
             tokens = "<s> " + tokens if append_bos else tokens
             tokens = tokens + " </s>" if append_eos else tokens
@@ -96,7 +108,7 @@ class ConditionalSequenceGenerator:
         input_lengths = torch.sum(input_ids != 1, dim=1).cuda()
 
         return input_ids, input_lengths
-    
+
     def tokenize_with_mask(self, input_str):
         """Tokenize sentence with a special <mask> token in it.
 
@@ -107,27 +119,35 @@ class ConditionalSequenceGenerator:
             input_ids (torch.Tensor): [batch_size, length]
             src_lengths (torch.Tensor): [batch_size]
         """
-        input_ids = self.tokenizer(input_str, return_tensors='pt', padding=True)['input_ids'].cuda()
+        input_ids = self.tokenizer(input_str, return_tensors="pt", padding=True)[
+            "input_ids"
+        ].cuda()
         input_lengths = torch.sum(input_ids != 1, dim=1).cuda()
         return input_ids, input_lengths
-    
+
     def encode_decode(self, src_input, tgt_input, mask_filling=False):
         """
         Args:
             src_input: (List[str])
             tgt_input: (List[str])
-            
+
         """
         if mask_filling:
             src_tokens, src_lengths = self.tokenize_with_mask(src_input)
-            prev_output_tokens, target, tgt_lengths = self.tokenize_target(tgt_input, left_pad=False, append_eos=True)
+            prev_output_tokens, target, tgt_lengths = self.tokenize_target(
+                tgt_input, left_pad=False, append_eos=True
+            )
         else:
             src_tokens, src_lengths = self.tokenize(src_input, append_bos=False)
-            prev_output_tokens, target, tgt_lengths = self.tokenize_target(tgt_input, left_pad=False)
-        
+            prev_output_tokens, target, tgt_lengths = self.tokenize_target(
+                tgt_input, left_pad=False
+            )
+
         with torch.no_grad():
             encoder_out = self.bart.model.encoder(src_tokens, src_lengths=src_lengths)
-            decoder_out = self.bart.model.decoder(prev_output_tokens, encoder_out=encoder_out, features_only=False)
+            decoder_out = self.bart.model.decoder(
+                prev_output_tokens, encoder_out=encoder_out, features_only=False
+            )
 
             probs = nn.functional.softmax(decoder_out[0], dim=-1)
             tgt_token_probs = torch.gather(probs, 2, target.unsqueeze(-1)).squeeze(2)
@@ -135,53 +155,59 @@ class ConditionalSequenceGenerator:
             # mask <pad> with probability 1.0
             max_tgt_length = tgt_lengths.max().item()
             tgt_lengths = tgt_lengths - 1
-            tgt_mask = torch.arange(max_tgt_length)[None, :].cuda() < tgt_lengths[:, None]
-            tgt_token_probs.masked_fill_(tgt_mask == False, 1.0)
+            tgt_mask = (
+                torch.arange(max_tgt_length)[None, :].cuda() < tgt_lengths[:, None]
+            )
+            tgt_token_probs.masked_fill_(not tgt_mask, 1.0)
 
         return tgt_token_probs, target
-    
+
     def generate(self, src_input, tgt_input=None):
         """Conditional generation.
-        
+
         Args:
             src_input (str or List[str]): input source sentence to be tokenized.
             tgt_input (str or List[str]): input target sentence to be tokenized.
         """
-        input_ids, lengths = self.tokenize(src_input, append_bos=False) 
-        
+        input_ids, lengths = self.tokenize(src_input, append_bos=False)
+
         target_ids = None
         if tgt_input is not None:
-            assert len(src_input) == len(tgt_input), "source & target length should match."
+            assert len(src_input) == len(tgt_input), (
+                "source & target length should match."
+            )
             target_ids, _ = self.tokenize(tgt_input, append_bos=False, left_pad=False)
-        
+
         with torch.no_grad():
             encoder_output = self.encode_sequence(input_ids, lengths)
-            decoder_output = self.decode_sequence(encoder_output, 
-                                                  target_ids=target_ids,
-                                                  prefix_tokens=[2])
+            decoder_output = self.decode_sequence(
+                encoder_output, target_ids=target_ids, prefix_tokens=[2]
+            )
         return decoder_output
-    
+
     def mask_filling(self, src_input, tgt_input=None):
         """
         Filling the mask in sentence(s).
         """
         input_ids, lengths = self.tokenize_with_mask(src_input)
-        
+
         target_ids = None
         if tgt_input is not None:
-            assert len(src_input) == len(tgt_input), "source & target length should match."
+            assert len(src_input) == len(tgt_input), (
+                "source & target length should match."
+            )
             target_ids, _ = self.tokenize(tgt_input, left_pad=False)
 
         with torch.no_grad():
             encoder_output = self.encode_sequence(input_ids, lengths)
-            decoder_output = self.decode_sequence(encoder_output, 
-                                                  target_ids=target_ids,
-                                                  prefix_tokens=[2, 0])
+            decoder_output = self.decode_sequence(
+                encoder_output, target_ids=target_ids, prefix_tokens=[2, 0]
+            )
         return decoder_output
-    
+
     def encode_sequence(self, input_ids, lengths):
         return self.bart.model.encoder(input_ids, src_lengths=lengths)
-        
+
     def decode_sequence(
         self,
         encoder_out,
@@ -192,20 +218,29 @@ class ConditionalSequenceGenerator:
         eos_id=2,
         prefix_tokens=[2, 0],
     ):
-        batch_size = encoder_out['encoder_padding_mask'][0].shape[0]
+        batch_size = encoder_out["encoder_padding_mask"][0].shape[0]
         init_input = torch.tensor([prefix_tokens] * batch_size, dtype=torch.long).cuda()
         token_probs, tokens = None, [[] for i in range(batch_size)]
         end_mask = torch.tensor([False] * batch_size).cuda()
 
         softmax = nn.Softmax(dim=1)
         for step in range(max_decode_step):
-            decoder_outputs = self.bart.model.decoder(init_input, encoder_out, features_only=False)
-            logits = decoder_outputs[0][:, -1, :]  # logits: [batch_size, vocab]
-            attn = decoder_outputs[1]['attn'][0]  # [batch_size, prev_token_len, src_token_len]
+            decoder_outputs = self.bart.model.decoder(
+                init_input, encoder_out, features_only=False
+            )
+            # logits: [batch_size, vocab]
+            logits = decoder_outputs[0][:, -1, :]
+            # attn: [batch_size, prev_token_len, src_token_len]
+            attn = decoder_outputs[1]["attn"][0]
 
             if step + 1 < min_decode_step:
-                logits[:, eos_id] = -math.inf  # mask <EOS> token when within minimal step
-            logits[:, pad_id], logits[:, 0] = -math.inf, -math.inf  # never select <PAD> & <BOS> token
+                logits[
+                    :, eos_id
+                ] = -math.inf  # mask <EOS> token when within minimal step
+            logits[:, pad_id], logits[:, 0] = (
+                -math.inf,
+                -math.inf,
+            )  # never select <PAD> & <BOS> token
             probs = softmax(logits)  # probs: [batch_size, vocab]
 
             # select tokens
@@ -217,17 +252,23 @@ class ConditionalSequenceGenerator:
 
             selected_token = selected_token.masked_fill(end_mask, pad_id)
             init_input = torch.cat([init_input, selected_token.unsqueeze(1)], dim=-1)
-            
+
             probs = torch.gather(probs, 1, selected_token.unsqueeze(1)).detach()
             probs = probs.masked_fill(end_mask.unsqueeze(1), 1.0)
-            
+
             # str & probability
-            token_probs = probs if token_probs is None else torch.cat([token_probs, probs], dim=-1)
+            token_probs = (
+                probs
+                if token_probs is None
+                else torch.cat([token_probs, probs], dim=-1)
+            )
             for t, s in zip(tokens, selected_token):
-                t.append(self.decode_func(s.unsqueeze(0)) if s.item() != pad_id else '<pad>')
-            
+                t.append(
+                    self.decode_func(s.unsqueeze(0)) if s.item() != pad_id else "<pad>"
+                )
+
             # stop generation when all finished
-            end_mask = torch.logical_or(end_mask, selected_token == eos_id) 
+            end_mask = torch.logical_or(end_mask, selected_token == eos_id)
             if end_mask.sum().item() == batch_size:
                 break
 
